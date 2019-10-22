197e05bdfb6c1b325bd1582c39810612d1e06d7e
Charity Parks <tuppercharity@gmail.com>
2019-10-22 01:52:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,13 +11,14 @@ class Scraper
     
     Students.each do |student|
     student.css("div.student-card a").each do |html|
-     student_name = html.css("h4.student_name").text 
-     student_loc = html.css("p.student_loc").text
+      student_name = html.css("h4.student_name").text 
+      student_loc = html.css("p.student_loc").text
+    end
   end
   
-    def self.scrape_profile_page(profile_url)
+  def self.scrape_profile_page(profile_url)
     
-    end
+  end
 
 end
 
