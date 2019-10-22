0f8d3cdf95b0b58b89189dc0dde10dddbd79d80a
Charity Parks <tuppercharity@gmail.com>
2019-10-22 01:46:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,19 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
+    doc = Nokogiri::HTML(html)
+    student = doc.css("div.roster-cards-container")
+    student = []
     
+    Students.each do |student|
+    student.css("div.student-card a").each do |html|
+      
   end
-
-  def self.scrape_profile_page(profile_url)
+  
+    def self.scrape_profile_page(profile_url)
     
-  end
+    end
 
 end
 
